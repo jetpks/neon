538211711d9f63752fcbec98393fffcbcb7fa513
Eric Jacobs <eric@analog.sh>
2015-04-11 18:08:38 -0600

    refactored how we do config.

--- Neon/neon_config.py
+++ Neon/neon_config.py
@@ -30,6 +30,7 @@
 import os
 import re
 import logging
+from neon_base import __call
 from neon_exceptions import NeonInstallFail
 
 logging.basicConfig(level=logging.DEBUG)
@@ -44,15 +45,13 @@ def config_shairport():
                 --log=/var/log/shairport.log --pidfile=/var/run/shairport')
 
 def config_alsa():
-    config = ''
     with open('/usr/share/alsa/alsa.conf', 'rw') as f:
         config = f.read()
         f.seek(0,0)
         f.write(re.sub(r'^pcm.front cards.pcm.front$', 'pcm.front cards.pcm.default', config))
         f.truncate()
 
-    if call('amixer cset numid=3 1') != 0:# todo get output from user
-        logging.warn("can not set analog as default output")
-
-    if call('amixer set PCM 1.00dB') != 0:
-        logging.warn("could not reset gain on DAC")
+    __call(['/usr/bin/amixer'], ['cset', 'numid=3', '1'],
+            fail_message="couldn't run amixer.")
+    __call(['/usr/bin/amixer'], ['set', 'PCM', '1.00dB'],
+            fail_message="couldn't run amixer.")
